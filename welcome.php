733efb0c3ac1197386e82439dd78129ba99b1fee
Lwalker79 <lwalker79@student.cccs.edu>
2022-11-20 17:25:35 -0700

    Final Submit

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your Email:<?php echo $_POST["email"]; ?><br>
+Thank you for your Feedback! <?php echo $_POST["feedback"]; ?>
+
+</body>
+</html> 
